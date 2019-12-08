503c4fb2f77909cf9b8ad52dd12443b598d2a7e2
Tsimafei Mikhailau <tsimafei_mikhailau@epam.com>
2019-12-08 15:48:09 +0300

    5-redux removed redundant

--- __tests__/HeaderContainer.spec.jsx
+++ __tests__/HeaderContainer.spec.jsx
@@ -6,17 +6,9 @@ import {reducer} from '../src/store/reducers/raducer.js';
 
 describe('HeaderContainer rendering', () => {
     const store = createStore(reducer);
-    it('renders if product is selected', () => {
-        const component = shallow(<HeaderContainer
-            isProductSelected={true}
-            store={store}
-        />);
-        expect(component).toMatchSnapshot();
-    });
 
     it('renders if product is not selected', () => {
         const component = shallow(<HeaderContainer
-            isProductSelected={false}
             store={store}
         />);
         expect(component).toMatchSnapshot();
